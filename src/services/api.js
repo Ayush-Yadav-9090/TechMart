8432ee058bf5ae16e1325346c19c57e87bdeaf3b
Ayush <ayush.y2966@gamil.com>
2026-03-08 22:33:02 +0530

    add gunicorn for railway deployment

--- src/services/api.js
+++ src/services/api.js
@@ -10,6 +10,7 @@ import axios from 'axios';
 // const API_BASE_URL = 'http://localhost:8000/api';
 const API_BASE_URL = 'https://techmart-production-3870.up.railway.app';
 
+
 // Create axios instance
 const api = axios.create({
   // baseURL: "http://localhost:8000/api",
